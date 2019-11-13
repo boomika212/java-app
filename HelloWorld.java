f3459c299a408b144f0213fd681d5d7673e7ba19
boomika212 <56113855+boomika212@users.noreply.github.com>
2019-11-13 12:45:43 +0530

    done successfull

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String args[]){
-        System.out.println("Hello Earthika");
+        System.out.println("Hello Earthika and say hi");
     }
 }
